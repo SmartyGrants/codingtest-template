c72764fdda9b9f56a352b52819ecb273d3ee1236
Christof Wildermuth <cwildermuth@flewidtech.com>
2024-05-06 10:25:31 +1000

    Implement getMaxSequenceLen and getMaxRepetitionCount; add additional test cases

--- passwordStrength/src/main/java/com/oc/codingtest/PasswordStrength.java
+++ passwordStrength/src/main/java/com/oc/codingtest/PasswordStrength.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
-import static java.util.stream.Collectors.*;
-
 public class PasswordStrength {
 
   private static final Logger log = LoggerFactory.getLogger(PasswordStrength.class);
@@ -25,11 +23,26 @@ public class PasswordStrength {
    *   eg3: "lucky" has a repetition count of 1 - each character appears only once.
    *   eg4: "Elephant" has a repetition count of 1 - as the two "e" characters have different cases (ie one "E", one "e")
    * The repetition count should be case-sensitive.
-   * @param password
-   * @return
+   * @param password The password to get the maximum repetition count of characters from
+   * @return The maximum repetition count as described above, 0 if the password is null or empty
    */
   public int getMaxRepetitionCount(String password) {
-    return 1;
+    if (password == null || password.isEmpty()) {
+      log.warn("Max Repetition Count: Password is null or empty, returning 0");
+      return 0;
+    }
+
+    // store the counts for each character in a map for quick access
+    // If we wanted to avoid the potential re-hashing the map for performance reasons we could calculate the initial
+    // capacity based on the password length but that seems like unnecessary optimisation
+    HashMap<Character, Integer> charCount = new HashMap<>();
+    for (char c : password.toCharArray()) {
+      int count = charCount.getOrDefault(c, 0) + 1; // increase the count for the character
+      charCount.put(c, count);
+    }
+
+    // find the max of all the counted values.
+    return Collections.max(charCount.values());
   }
 
   /**
@@ -41,12 +54,53 @@ public class PasswordStrength {
    * eg3b: "passwordABC" would have a max. sequence length of 3 - for the sequence of "ABC".
    * eg4: "AbCdEf" would have a sequence length of 6, even though it is mixed case.
    * eg5: "ABC_DEF" would have a sequence length of 3, because the special character breaks the progression
-   * Check the supplied password.  Return true if the repetition count and sequence length are below or equal to the
-   * specified maximum.  Otherwise, return false.
-   * @param password
-   * @return
+   * @param password The password to get the maximum sequence length of characters from
+   * @return The maximum sequence length as described above, 0 if the password is null or empty
    */
   public int getMaxSequenceLen(String password) {
-    return 0;
+    if (password == null || password.isEmpty()) {
+      log.warn("Max Sequence Length: Password is null or empty, returning 0");
+      return 0;
+    }
+
+    // It's possible to find the max sequence from left to right and right to left in
+    // a single loop without having to reverse the password String. But that is harder to read and follow.
+    // This could always be done if the absolute maximum performance was a requirement but given that passwords
+    // typically are between 6 and 20 characters long, the performance impact should be negligible.
+    int maxSequenceLengthLeftToRight = findMaxSequenceLengthLeftToRight(password);
+    int maxSequenceLengthRightToLeft = findMaxSequenceLengthLeftToRight(reverseString(password));
+
+    return Math.max(maxSequenceLengthLeftToRight, maxSequenceLengthRightToLeft); // determine the longer sequence
+  }
+
+  private int findMaxSequenceLengthLeftToRight(String password) {
+    // no need to check the password for null or empty again as this method is
+    // only called internally from where these conditions already have been checked
+
+    int maxSeqLength = 0;
+    int currentSeqLength = 0;
+    char prevChar = password.charAt(0);
+    for (char currentChar : password.toCharArray()) {
+      if (Character.isLetterOrDigit(currentChar)) {
+        // either increase the current sequence count or start a new one
+        currentSeqLength = isSequentialIgnoreCase(prevChar, currentChar) ? currentSeqLength + 1 : 1;
+      } else {
+        currentSeqLength = 0; // reset the current sequence count as it was interrupted by a special character
+      }
+      maxSeqLength = Math.max(maxSeqLength, currentSeqLength); // determine the maximum sequence count so far
+      prevChar = currentChar;
+    }
+    return maxSeqLength;
+  }
+
+  private boolean isSequentialIgnoreCase(char previous, char current) {
+    // use the fact that in the ASCII table, characters have corresponding numerical values
+    int diff = Character.toLowerCase(current) - Character.toLowerCase(previous);
+    return diff == 1;
+  }
+
+  private String reverseString(String str) {
+    StringBuilder sb = new StringBuilder(str);
+    return sb.reverse().toString();
   }
 }
--- passwordStrength/src/test/java/com/oc/codingtest/PasswordStrengthTest.java
+++ passwordStrength/src/test/java/com/oc/codingtest/PasswordStrengthTest.java
@@ -23,7 +23,21 @@ class PasswordStrengthTest {
       Arguments.of("password", 2),
       Arguments.of("touchwood", 3),
       // The check should be case-sensitive
-      Arguments.of("TheQuickBrownFoxJumpsOverTheLazyDog", 3)
+      Arguments.of("TheQuickBrownFoxJumpsOverTheLazyDog", 3),
+
+      //Additional test cases
+      Arguments.of("", 0), // empty String
+      Arguments.of(null, 0), // null
+      Arguments.of("1", 1), // single digit
+      Arguments.of("a", 1), // single letter
+      Arguments.of("11111", 5), // same digits only
+      Arguments.of("aaaaa", 5), // same letters only
+      Arguments.of("abababababab", 6), // same max count of different letters
+      Arguments.of("Aa", 1), // same letters mixed case
+      Arguments.of("[", 1), // single special character
+      Arguments.of("[[[}}}}", 4), // only special characters
+      Arguments.of("[1[2[3[4[", 5), // special characters with digits
+      Arguments.of("[a[b[c[d[", 5) // special characters with letters
     );
   }
 
@@ -55,7 +69,29 @@ class PasswordStrengthTest {
       Arguments.of("01234*567:", 5),
 
       //Ascending and descending mixed
-      Arguments.of("23454321", 5)
+      Arguments.of("23454321", 5),
+
+      //Additional test cases
+      Arguments.of("", 0), // empty String
+      Arguments.of(null, 0), // null
+      Arguments.of("/*_-", 0), // only special characters
+      Arguments.of("_a_", 1), // single letter lowercase surrounded by special characters
+      Arguments.of("_A_", 1), // single letter uppercase surrounded by special characters
+      Arguments.of("_1_", 1), // single digit surrounded by special characters
+      Arguments.of("_12_", 2), // digit sequence surrounded by special characters
+      Arguments.of("a", 1), // single letter lowercase
+      Arguments.of("A", 1), // single letter uppercase
+      Arguments.of("0", 1), // single digit
+      Arguments.of("BcDeDcBa", 5), // ascending/descending mixed with mixed case
+      Arguments.of("123456_54321", 6), // ascending/descending digits only with ascending max
+      Arguments.of("12345_654321", 6), // ascending/descending digits only with descending max
+      Arguments.of("aBcDe_dCbA", 5), // ascending/descending letter only with ascending max
+      Arguments.of("aBcD_eDcBa", 5), // ascending/descending letter only with descending max
+      Arguments.of("@AB", 2), // uppercase sequence according to ASCII table starting with special character
+      Arguments.of("YZ[", 2), // uppercase sequence according to ASCII table ending with special character
+      Arguments.of("za", 1), // wrap-around shouldn't be considered sequential
+      Arguments.of("123456ABCDE", 6), // digits and uppercase sequence mixed
+      Arguments.of("123_abcd_56789_efghijklmno_ab_4321", 11) // multiple sequences
     );
   }
 
